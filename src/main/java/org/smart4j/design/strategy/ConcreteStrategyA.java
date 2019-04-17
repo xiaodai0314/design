e3e70454d8e311706801588b925f72be84c663e0
xiaodai0314 <411500916@qq.com>
2019-04-17 15:46:20 +0800

    策略模式 strategy 策略模式+简单工厂模式 simplefactoryandstrategy

---
+++ src/main/java/org/smart4j/design/strategy/ConcreteStrategyA.java
@@ -0,0 +1,8 @@
+package org.smart4j.design.strategy;
+
+public class ConcreteStrategyA implements Strategy {
+    @Override
+    public void AlgorithmInterface() {
+        System.out.println("ConcreteStrategyA");
+    }
+}
